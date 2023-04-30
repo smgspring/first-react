bd3d8bc01e210f3f2b53c38e54f144b485f65cf0
smgspring <smg02011224@gmail.com>
2023-04-30 14:54:52 +0900

    1-fetch
    not completed to use ESlint, prettier

---
+++ basic_crud/src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <ol>
+      <li>
+        <a href="/read/1">html</a>
+      </li>
+      <li>
+        <a href="/read/2">css</a>
+      </li>
+      <li>
+        <a href="/read/3">js</a>
+      </li>
+    </ol>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </>
+  );
+}
+
+export default App;
